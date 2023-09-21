62f89eca23c1bb57413e1069297e6ea01674582c
Jared <jaredbastarracheas@gmail.com>
2023-09-21 11:15:25 -0600

    Agregar archivos de proyecto.

---
+++ MatricesB/Matriz.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace MatricesB
+{
+
+    public class Matriz
+        {
+            private int[,] matriz;
+            private int filas;
+            private int columnas;
+
+            public Matriz(int[,] matriz)
+            {
+                this.matriz = matriz;
+                this.filas = matriz.GetLength(0);
+                this.columnas = matriz.GetLength(1);
+            }
+            public Matriz(Matriz segundaMatriz)
+            {
+                this.filas = segundaMatriz.filas;
+                this.columnas = segundaMatriz.columnas;
+                this.matriz = new int[filas, columnas];
+                Array.Copy(segundaMatriz.matriz, this.matriz, filas * columnas);
+            }
+
+            public void MostrarMatriz()
+            {
+                for (int i = 0; i < filas; i++)
+                {
+                    for (int j = 0; j < columnas; j++)
+                    {
+                        Console.Write(matriz[i, j].ToString().PadLeft(5)); 
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            public Matriz Sumar(Matriz segundaMatriz)
+            {
+                if (filas == segundaMatriz.filas && columnas == segundaMatriz.columnas)
+                {
+                    int[,] resultado = new int[filas, columnas];
+                    for (int i = 0; i < filas; i++)
+                    {
+                        for (int j = 0; j < columnas; j++)
+                        {
+                            resultado[i, j] = matriz[i, j] + segundaMatriz.matriz[i, j];
+                        }
+                    }
+                    return new Matriz(resultado);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            public Matriz Restar(Matriz segundaMatriz)
+            {
+                if (filas == segundaMatriz.filas && columnas == segundaMatriz.columnas)
+                {
+                    int[,] resultado = new int[filas, columnas];
+                    for (int i = 0; i < filas; i++)
+                    {
+                        for (int j = 0; j < columnas; j++)
+                        {
+                            resultado[i, j] = matriz[i, j] - segundaMatriz.matriz[i, j];
+                        }
+                    }
+                    return new Matriz(resultado);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            public Matriz Multiplicar(Matriz segundaMatriz)
+            {
+                if (filas == segundaMatriz.filas && columnas == segundaMatriz.columnas)
+                {
+                    int[,] resultado = new int[filas, columnas];
+                for (int i = 0; i < filas; i++)
+                {
+                    for (int j = 0; j < columnas; j++)
+                    {
+                        resultado[i, j] = matriz[i, j] * segundaMatriz.matriz[i, j];
+                    }
+                }
+                return new Matriz(resultado);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            public Matriz División(Matriz segundaMatriz)
+            {
+                if (filas == segundaMatriz.filas && columnas == segundaMatriz.columnas)
+                {
+                    int[,] resultado = new int[filas, columnas];
+                    for (int i = 0; i < filas; i++)
+                    {
+                        for (int j = 0; j < columnas; j++)
+                        {
+                            resultado[i, j] = matriz[i, j] / segundaMatriz.matriz[i, j];
+                        }
+                    }
+                    return new Matriz(resultado);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+
+        }
+}
+
---
+++ MatricesB/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Globalization;
+
+namespace MatricesB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Elige una opción:");
+                Console.WriteLine("1. Calcular matrices");
+                Console.WriteLine("2. Salir");
+                int opcionPrincipal = Convert.ToInt32(Console.ReadLine());
+
+                if (opcionPrincipal == 2)
+                    break;
+
+                if (opcionPrincipal != 1)
+                {
+                    Console.WriteLine("Opción no válida. Por favor, elige una opción válida.");
+                    continue;
+                }
+
+                Console.WriteLine("Ingresa la primera matriz:");
+                Matriz matriz1 = CrearMatrizCO();
+
+                Console.WriteLine("Presiona cualquier tecla para ingresar la segunda matriz.");
+                Console.ReadKey();
+                Console.Clear();
+
+                Console.WriteLine("Ingresa la segunda matriz:");
+                Matriz matriz2 = CrearMatrizCO();
+                Matriz matrizOriginal1 = new Matriz(matriz1);
+                Matriz matrizOriginal2 = new Matriz(matriz2);
+
+                Console.Clear();
+                Console.WriteLine($"Matriz 1:");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                matriz1.MostrarMatriz();
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Matriz 2:");
+                Console.ForegroundColor = ConsoleColor.Green;
+                matriz2.MostrarMatriz();
+                Console.ForegroundColor = ConsoleColor.White;
+
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Elige una operación:");
+                    Console.WriteLine("1. Suma");
+                    Console.WriteLine("2. Resta");
+                    Console.WriteLine("3. Multiplicación");
+                    Console.WriteLine("4.- Dividir");
+                    Console.WriteLine("<--------------------------------->".PadLeft(80));
+                    Console.WriteLine("5. Regresar a matriz original");
+                    Console.WriteLine("6. Calcular otras matrices");
+                    Console.WriteLine("7. Salir");
+                    int opcion = Convert.ToInt32(Console.ReadLine());
+
+                    if (opcion ==7)
+                        break;
+
+                    if (opcion == 5)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        matriz1 = new Matriz(matrizOriginal1);
+                        matriz2 = new Matriz(matrizOriginal2);
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Matrices restauradas!".PadLeft(80));
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine($"Matriz 1:");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        matriz1.MostrarMatriz();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Matriz 2:");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        matriz2.MostrarMatriz();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
+
+                    if (opcion == 6)
+                        break;
+
+                    Matriz resultado = RealizarOperacion(matriz1, matriz2, opcion);
+                    if (resultado != null)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Resultado:".PadLeft(60));
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        resultado.MostrarMatriz();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Selecciona una opción correcta");
+                    }
+                }
+            }
+        }
+
+        static Matriz CrearMatrizCO()
+        {
+            Console.WriteLine("Número 1: ");
+            int elemento1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Número 2: ");
+            int elemento2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Número 3: ");
+            int elemento3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Número 4: ");
+            int elemento4 = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matriz = { { elemento1, elemento2 },
+                              { elemento3, elemento4 } };
+            return new Matriz(matriz);
+        }
+
+        static Matriz RealizarOperacion(Matriz matriz1, Matriz matriz2, int opcion)
+        {
+            switch (opcion)
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return matriz1.Sumar(matriz2);
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return matriz1.Restar(matriz2);
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return matriz1.Multiplicar(matriz2);
+                case 4:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return matriz1.División(matriz2);
+
+                default:
+                    return null;
+            }
+        }
+    }
+
+
+}
